eb5412e036f69ab1ba271c23c367989bce1cc9a7
P Gergő <69343477+Rav3n95@users.noreply.github.com>
2022-03-25 12:39:54 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ game         'gta5'
 name            'gp-traders'
 author          'Rav3n95#2849 - https://github.com/Rav3n95'
 description     'Simple trader system'
-version         '1.0.4'
+version         '1.0.5'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
